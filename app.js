06d300b87463196e59d1a2475d06ad7537c425bd
Kazi Jamal <jamalkazi02@gmail.com>
2020-09-25 18:29:30 -0400

    Creater starter Node.js app

---
+++ app.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const express = require("express");
+const nunjucks = require("nunjucks");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+
+const app = express();
+
+app.use(express.json());
+
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app,
+});
+
+const db = process.env.MONGO_URI;
+mongoose
+    .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
+
+const secret = process.env.SECRET;
+app.use(
+    session({
+        secret: secret,
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+app.get("/", function (req, res) {
+    res.render("index.html");
+});
+
+app.use(function (req, res, next) {
+    res.status(404).send("Sorry can't find that!");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, function () {
+    console.log(`Server started on ${port}`);
+});
