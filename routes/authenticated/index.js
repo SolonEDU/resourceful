08b94869e23e7ee884982798825892182857c19b
ray. lee <2lee.raymond@gmail.com>
2020-09-25 22:26:02 -0500

    Add routes to index.js

--- routes/authenticated/index.js
+++ routes/authenticated/index.js
@@ -4,9 +4,11 @@ const { ensureAuthenticated } = require("../../config/auth");
 
 router.use(ensureAuthenticated);
 
+router.use("/comments", require("./comments"));
 router.use("/dashboard", require("./dashboard"));
 router.use("/resources", require("./resources"));
 router.use("/topics", require("./topics"));
 router.use("/savedresources", require("./savedResources"));
+router.use("/votes", require("./votes"));
 
 module.exports = router;
