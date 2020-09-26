829ed8dc40bf0d4583d32d9719377fc2168f3725
Kazi Jamal <jamalkazi02@gmail.com>
2020-09-26 12:51:43 -0400

    Redirect already authenticated users from unauth to auth routes

--- routes/unauthenticated/index.js
+++ routes/unauthenticated/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const router = express.Router();
+const { alreadyAuthenticated } = require("../../config/auth");
+
+router.use(alreadyAuthenticated);
 
 router.use("/dashboard", require("./dashboard"));
 router.use("/resources", require("./resources"));
